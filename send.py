b61f690f8fa4d8533529166eb7c45a98e94b6731
Sara Ghaemi <ghaemi.sr@gmail.com>
2016-06-24 11:51:08 +0430

    second commit

--- send.py
+++ send.py
@@ -1,12 +1,8 @@
 import socket
 
-UDP_PORT = 7000
-MESSAGE = "Salam Jigare man!!"
 
-print("UDP target port:", UDP_PORT)
-print("message:", MESSAGE)
-
-sock = socket.socket(socket.AF_INET,  # Internet
-                     socket.SOCK_DGRAM)  # UDP
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-sock.sendto(MESSAGE.encode(), ('<broadcast>', UDP_PORT))
+def send(port, message):
+    sock = socket.socket(socket.AF_INET,  # Internet
+                         socket.SOCK_DGRAM)  # UDP
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    sock.sendto(message.encode(), ('<broadcast>', port))
